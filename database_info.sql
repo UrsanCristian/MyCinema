e38de3568a46a8ccb016b6958735b3b08938d484
Ursan Cristian <cristian.ursan99@gmail.com>
2024-02-23 16:55:19 +0200

    Created backend structure (v1)

--- database_info.sql
+++ database_info.sql
@@ -1,6 +1,7 @@
 CREATE TABLE movies (
 	id SERIAL PRIMARY KEY,
 	imbd VARCHAR(20),
+    title VARCHAR(100) NOT NULL,
     release_year INT NOT NULL,
     genre VARCHAR(255) NOT NULL,
     director VARCHAR(100) NOT NULL,
@@ -14,6 +15,7 @@ CREATE TABLE movies (
 CREATE TABLE series (
 	id SERIAL PRIMARY KEY,
 	imbd VARCHAR(20),
+    title VARCHAR(100) NOT NULL,
     release_year INT NOT NULL,
     genre VARCHAR(255) NOT NULL,
     seasons INT NOT NULL,
@@ -23,4 +25,18 @@ CREATE TABLE series (
     country VARCHAR(100) NOT NULL,
     my_score DECIMAL(2,1) NOT NULL,
     my_review TEXT NOT NULL 
+);
+
+INSERT INTO movies (imbd, title, release_year, genre, director, actors, about, country, my_score, my_review)
+VALUES (
+    'tt1790864',
+    'The Maze Runner',
+    2014,
+    'Action, Mystery, Sci-Fi',
+    'Wes Ball',
+    'Dylan O Brien, Kaya Scodelario, Will Poulter',
+    'Thomas is deposited in a community of boys after his memory is erased, soon learning they are all trapped in a maze that will require him to join forces with fellow runners for a shot at escape.',
+    'United Kingdom, United States',
+    8.4,
+    'Great Movie'
 );
--- index.js
+++ index.js
@@ -4,8 +4,51 @@ import bodyParser from "body-parser";
 import pg from "pg";
 import "dotenv/config";
 
-
 const app = express();
 const port = 3000;
 
+const key = process.env.apikey;
+const db_pass = process.env.db_pass;
+const db_name = "ChrisMovies";
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: db_name,
+  password: db_pass,
+  port: 5432,
+});
+
+db.connect();
+
+let moviesTableName = "movies";
+let seriesTableName = "series";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let selectedType = moviesTableName;
+
+let posts;
+
+async function getMoviesOrSeries(type) {
+  const dataQuery = await db.query(`SELECT * FROM ${type}`);
+  posts = dataQuery.rows;
+}
+
+app.get("/", async (req, res) => {
+  await getMoviesOrSeries(selectedType);
+  res.render("index.ejs", { posts: posts });
+});
+
+app.post("/movies", async (req, res) => {
+    selectedType = moviesTableName;
+});
+
+app.post("/series", async (req, res) => {
+    selectedType = seriesTableName;
+});
+
+app.listen(port, () => {
+    console.log(`Server Running on Port ${port}`);
+});
